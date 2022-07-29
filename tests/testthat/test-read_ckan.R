fb061d6f7cd547e38eaab966aca56253fec438ad
Matt Johnson <mrjoh3@gmail.com>
2022-07-29 17:37:57 +1000

    add tests

--- tests/testthat/test-read_ckan.R
+++ tests/testthat/test-read_ckan.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+ulst <- as.list.environment(urls)
+
+test_that("urls are current", {
+
+  for (u in 1:length(ulst)){
+    expect_true(RCurl::url.exists(ulst[[u]]))
+  }
+
+})
+
+
+
+
+test_that("each ckan repo can be searched", {
+
+  for (n in names(ulst)){
+    search <- do.call(paste0('search_ckan_', n), args = list(search_term = 'schools'))
+    expect_s3_class(search, 'data.frame')
+  }
+
 })
